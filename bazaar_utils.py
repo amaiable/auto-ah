01190ec6c7d3e7b2373666b42ca588c14c8b00dd
amaiable <atmai2002@gmail.com>
2024-06-13 16:25:58 +1000

    FIX/FEAT: Restructured previous code surrounding API calls and added functionality for querying for prices of products

--- bazaar_utils.py
+++ bazaar_utils.py
@@ -3,16 +3,27 @@ from heapq import heappush, heappop
 
 from api_utils import make_get_request
 
+BAZAAR_ENDPOINT = "https://api.hypixel.net/v2/skyblock/bazaar"
+
 class BazaarUtil:
     def __init__(self, api_key: str):
         self.api_key = api_key
 
-        self.bazaar_item_names = []
-        bazaar_data = make_get_request(f"https://api.hypixel.net/v2/skyblock/bazaar")
-        for product_name in bazaar_data["products"]:
-            self.bazaar_item_names.append(product_name)
+        self.bazaar_data = {}
+        self.bazaar_produtcs = []
+        self.bazaar_item_names = set()
+        self.refresh_bazaar_data()
 
-    def search_items(self, query: str, num_results: int = 5):
+    def refresh_bazaar_data(self) -> None:
+        self.bazaar_data = make_get_request(BAZAAR_ENDPOINT)
+        self.bazaar_products = self.bazaar_data["products"]
+        self.bazaar_item_names = set(self.bazaar_products.keys())
+
+    def search_item_names(self, query: str, num_results: int = 5) -> List[str]:
+        """
+        Uses edit distance DP algorithm to find the num_results items with the most similar name
+        TODO: edit distance might not be the best algorithm for this, as it may favor shorter (but incorrect) names
+        """
 
         # TODO: Need to unit test this
         def edit_distance(s1: str, s2: str) -> int:
@@ -34,9 +45,53 @@ class BazaarUtil:
 
         for item_name in self.bazaar_item_names:
             item_name = item_name.lower()
-            heappush(word_distances, (edit_distance(query, item_name)))
+            word_distance = edit_distance(query, item_name)
+
+            if word_distance == 0:
+                return [item_name]
+
+            heappush(word_distances, (-word_distance, item_name))
             if len(results) > k:
                 heappop(word_distances)
 
         return [item_name for _, item_name in word_distances]
 
+    def get_item_prices(self, query: str) -> Dict[str, float]:
+
+        if query not in self.bazaar_item_names:
+            raise KeyError(f"Cannot find item: '{query}'. Perhaps you meant one of {self.search_item_names(query)}")
+
+        self.refresh_bazaar_data()
+
+        bid_price = self.bazaar_products[query]["quick_status"]["sellPrice"]
+        ask_price = self.bazaar_products[query]["quick_status"]["buyPrice"]
+
+        return {"bid": bid_price, "ask": ask_price}
+
+    def get_item_top_order_data(self, query: str) -> Dict[str, List[Dict[str, float]]]:
+
+        if query not in self.bazaar_item_names:
+            raise KeyError(f"Cannot find item: '{query}'. Perhaps you meant one of {self.search_item_names(query)}")
+
+        self.refresh_bazaar_data()
+
+        bid_orders = self.bazaar_products[query]["sell_summary"]
+        ask_orders = self.bazaar_products[query]["buy_summary"]
+
+        # Clean data (remove unnecessary data and change key names)
+        def clean_order(raw_order_info: Dict[str, float]) -> Dict[str, float]:
+            cleaned_order_info = {}
+            cleaned_order_info["quantity"] = raw_order_info["amount"]
+            cleaned_order_info["unit_price"] = raw_order_info["pricePerUnit"]
+            return cleaned_order_info
+
+        bid_orders = [cleaned_order_info(raw_order_info) for raw_order_info in bid_orders]
+        ask_orders = [cleaned_order_info(raw_order_info) for raw_order_info in ask_orders]
+        
+        return {"bid_orders": bid_orders, "ask_orders": ask_orders}
+
+    def find_mean_reversion_flips(self) -> any:
+        """
+        Using recent historical data, find short term flip from expected mean reverting price behaviour
+        """
+        pass
