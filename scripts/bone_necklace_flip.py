eb807a4bc3fcf4f28cf7eef9141915ebfaa0fda3
amaiable <atmai2002@gmail.com>
2024-06-20 12:52:34 -0700

    FEAT: Using new AuctionHouseUtil to find an approximated profit for bone necklace flipping

--- scripts/bone_necklace_flip.py
+++ scripts/bone_necklace_flip.py
@@ -1,14 +1,19 @@
 from time import time
 
 import os
+from dotenv import load_dotenv
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../utils')))
 from bazaar_utils import BazaarUtil
+from auction_house_utils import AuctionHouseUtil
 
 INSTANT_FLAG = False
 
 def main():
     bz_util = BazaarUtil()
+
+    load_dotenv()
+    ah_util = AuctionHouseUtil(os.getenv("API_KEY"))
 
     spirit_bone_prices = bz_util.get_item_prices("Spirit Bone")
     spirit_wing_prices = bz_util.get_item_prices("Spirit Wing")
@@ -25,7 +30,11 @@ def main():
                             + 96 * soul_string_prices[price_tag] + 8 * thorn_fragment_prices[price_tag] \
                             + 370 * essence_wither_prices[price_tag] + 35000
 
-    print(buy_all_price)
+    curr_listings = ah_util.get_auctions_by_item_name("Bone Necklace", bin_flag=True, modifiers=["✪✪✪✪✪"])
+    min_listing = min(curr_listings, key=lambda x: x["starting_bid"])
+    curr_min_price = min_listing["starting_bid"]
+
+    print("PROFIT EXCLUDING TAX IS",  curr_min_price - buy_all_price)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "INSTANT":
