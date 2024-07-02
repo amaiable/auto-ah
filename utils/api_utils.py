47fb2cec8887fa7b67ed0b03ff6952dfdbf7ffe5
amaiable <atmai2002@gmail.com>
2024-07-02 15:38:08 -0700

    FEAT: Added generalised post request function to API utils

--- utils/api_utils.py
+++ utils/api_utils.py
@@ -14,10 +14,29 @@ def make_get_request(endpoint: str) -> Dict[any, any]:
         try:
             response = requests.get(endpoint)
             response.raise_for_status()
-            return response.json()
+            json_data = response.json()
+            if not json_data["success"]:
+                raise Exception("Unsuccessful API call")
+            return json_data
         except requests.RequestException:
             time.sleep(retry_delay)
             retry_delay *= 2
             retry_delay += random.uniform(0, 1)
 
-    return Exception("Maximum retry attempts have been made with no success")
+    raise Exception("Maximum retry attempts have been made with no success")
+
+def make_post_request(endpoint: str, data: Dict[any, any]) -> Dict[any, any]:
+
+    retry_delay = 1
+
+    for attempt in range(0, MAX_RETRIES):
+        try:
+            response = requests.post(endpoint, data=data)
+            json_data = response.json()
+            return json_data
+        except requests.RequestException:
+            time.sleep(retry_delay)
+            retry_delay *= 2
+            retry_delay += random.uniform(0, 1)
+
+    raise Exception("Maximum retry attempts have been made with no success")
